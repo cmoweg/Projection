5f17515721555659f946bb5b1214752e4b3a5e83
jeonghani <wjdgksdl97@naver.com>
2021-08-16 23:11:55 +0900

    FE detail

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,5 +10,13 @@ router.get('/', function (req, res, next) {
   var obj = JSON.parse(fs.readFileSync('data/post_intro.json', 'utf8'));
   res.render('index/index', { "post_intro": obj, "isLogin": false });
 });
+
+exports.index = function(req, res){
+ conn.query('select * from Test', function (err, rows, fields) {
+  if (err) throw err; 
+  console.log(rows);
+  res.render('index', { title: 'select * from Test -> rows.length : ' + rows.length });
+ });
+};
 
 module.exports = router;
