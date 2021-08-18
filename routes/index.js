aea848a885d1ff5763d37e5bca4b44dc7094d6a3
jeonghani <wjdgksdl97@naver.com>
2021-08-18 14:06:29 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
   // https://stackoverflow.com/a/10011078
   var obj = JSON.parse(fs.readFileSync('data/post_intro.json', 'utf8'));
-  res.render('index/index', { "post_intro": obj, "isLogin": false });
+  res.render('index/index', { "post_intro": obj, "authenticate": req.session.authenticate });
 });
 
 module.exports = router;
